b1416b05c04bdcec3be816f81936920034982bea
duan <wddwyss@gmail.com>
2020-05-29 23:26:02 +0900

    binary-tree-inorder-traversal

---
+++ binary-tree-inorder-traversal/src/main.rs
@@ -0,0 +1,51 @@
+use std::cell::RefCell;
+use std::rc::Rc;
+
+// Definition for a binary tree node.
+#[derive(Debug, PartialEq, Eq)]
+pub struct TreeNode {
+    pub val: i32,
+    pub left: Option<Rc<RefCell<TreeNode>>>,
+    pub right: Option<Rc<RefCell<TreeNode>>>,
+}
+
+impl TreeNode {
+    #[inline]
+    pub fn new(val: i32) -> Self {
+        TreeNode {
+            val,
+            left: None,
+            right: None,
+        }
+    }
+}
+
+struct Solution;
+impl Solution {
+    pub fn inorder_traversal(root: Option<Rc<RefCell<TreeNode>>>) -> Vec<i32> {
+        let root = match root {
+            None => return vec![],
+            Some(a) => a,
+        };
+        let mut stack = vec![(root, false)];
+        let mut result = vec![];
+        while let Some((node, left_seen)) = stack.pop() {
+            if left_seen {
+                result.push(node.borrow().val);
+                if let Some(right) = node.borrow().right.clone() {
+                    stack.push((right, false))
+                }
+            } else {
+                stack.push((node.clone(), true));
+                if let Some(left) = node.borrow().left.clone() {
+                    stack.push((left, false))
+                }
+            }
+        }
+        result
+    }
+}
+
+fn main() {
+    println!("Hello, world!");
+}
