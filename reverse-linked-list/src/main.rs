34cc2b558fbdae6f03745e11c9b6abb76956a135
duan <wddwyss@gmail.com>
2020-11-30 22:29:46 +0900

    Add iterative solution

--- reverse-linked-list/src/main.rs
+++ reverse-linked-list/src/main.rs
@@ -4,24 +4,40 @@ pub struct ListNode {
     pub next: Option<Box<ListNode>>,
 }
 
-use std::mem;
-
 pub struct Solution {}
-impl Solution {
-    fn helper(
-        head: Option<Box<ListNode>>,
-        reversed: Option<Box<ListNode>>,
-    ) -> Option<Box<ListNode>> {
-        if let Some(mut node) = head {
-            let new_head = mem::replace(&mut node.next, reversed);
-            Self::helper(new_head, Some(node))
-        } else {
-            reversed
-        }
-    }
 
+// MARK: Recursively: Time: O(n), Space: O(n)
+// Space O(n) is due to call stack size of recursion
+
+// impl Solution {
+//     fn helper(
+//         head: Option<Box<ListNode>>,
+//         reversed: Option<Box<ListNode>>,
+//     ) -> Option<Box<ListNode>> {
+//         if let Some(mut node) = head {
+//             let new_head = std::mem::replace(&mut node.next, reversed);
+//             Self::helper(new_head, Some(node))
+//         } else {
+//             reversed
+//         }
+//     }
+
+//     pub fn reverse_list(head: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
+//         Self::helper(head, None)
+//     }
+// }
+
+// MARK: Iteratively: Time: O(n), Space: O(1)
+
+impl Solution {
     pub fn reverse_list(head: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
-        Self::helper(head, None)
+        let mut head = head;
+        let mut res = None;
+        while let Some(mut node) = head {
+            head = std::mem::replace(&mut node.next, res);
+            res = Some(node);
+        }
+        res
     }
 }
 
