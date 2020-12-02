8410d07b78c6c2fd94e2e1f10fc0dfc3de4f48ff
duan <wddwyss@gmail.com>
2020-12-02 22:10:46 +0900

    adopt recursive

--- merge-two-sorted-lists/src/main.rs
+++ merge-two-sorted-lists/src/main.rs
@@ -12,42 +12,25 @@ impl ListNode {
     }
 }
 
-pub fn merge_two_lists(
-    l1: Option<Box<ListNode>>,
-    l2: Option<Box<ListNode>>,
-) -> Option<Box<ListNode>> {
-    let mut cur1 = l1;
-    let mut cur2 = l2;
-
-    let mut dummy_head = ListNode::new(0);
-    let mut cur = &mut dummy_head;
-
-    loop {
-        match (cur1.take(), cur2.take()) {
-            (Some(a), Some(b)) => {
-                if a.val < b.val {
-                    cur.next = Some(Box::new(ListNode::new(a.val)));
-                    cur = cur.next.as_mut().unwrap();
-                    cur1 = a.next;
-                    cur2 = Some(b);
+pub struct Solution {}
+impl Solution {
+    pub fn merge_two_lists(
+        l1: Option<Box<ListNode>>,
+        l2: Option<Box<ListNode>>,
+    ) -> Option<Box<ListNode>> {
+        match (l1, l2) {
+            (None, None) => None,
+            (Some(a), None) => Some(a),
+            (None, Some(b)) => Some(b),
+            (Some(mut a), Some(mut b)) => {
+                if a.val > b.val {
+                    b.next = Self::merge_two_lists(b.next, Some(a));
+                    return Some(b);
                 } else {
-                    cur.next = Some(Box::new(ListNode::new(b.val)));
-                    cur = cur.next.as_mut().unwrap();
-                    cur1 = Some(a);
-                    cur2 = b.next;
+                    a.next = Self::merge_two_lists(a.next, Some(b));
+                    return Some(a);
                 }
             }
-            (Some(a), None) => {
-                cur.next = Some(Box::new(ListNode::new(a.val)));
-                cur = cur.next.as_mut().unwrap();
-                cur1 = a.next;
-            }
-            (None, Some(b)) => {
-                cur.next = Some(Box::new(ListNode::new(b.val)));
-                cur = cur.next.as_mut().unwrap();
-                cur2 = b.next;
-            }
-            _ => return dummy_head.next,
         }
     }
 }
