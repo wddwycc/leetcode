db43f3d2b0385954e6f4715045f89636a84a4aec
duan <wddwyss@gmail.com>
2020-12-02 22:32:27 +0900

    opt

--- k-th-symbol-in-grammar/src/lib.rs
+++ k-th-symbol-in-grammar/src/lib.rs
@@ -1,17 +1,32 @@
+use std::collections::HashMap;
+
 pub struct Solution {}
 impl Solution {
+    pub fn cached_kth_grammar(n: i32, k: i32, cache: &mut HashMap<(i32, i32), i32>) -> i32 {
+        if let Some(cached) = cache.get(&(n, k)) {
+            return *cached;
+        }
+        let res = {
+            if n == 1 {
+                0
+            } else {
+                let prev_k = (k - 1) / 2 + 1;
+                match (Self::cached_kth_grammar(n - 1, prev_k, cache), k % 2) {
+                    (0, 0) => 1,
+                    (0, 1) => 0,
+                    (1, 0) => 0,
+                    (1, 1) => 1,
+                    _ => panic!(),
+                }
+            }
+        };
+        cache.insert((n, k), res);
+        res
+    }
+
     pub fn kth_grammar(n: i32, k: i32) -> i32 {
-        if n == 1 {
-            return 0;
-        }
-        let prev_k = (k - 1) / 2 + 1;
-        match (Self::kth_grammar(n - 1, prev_k), k % 2) {
-            (0, 0) => 1,
-            (0, 1) => 0,
-            (1, 0) => 0,
-            (1, 1) => 1,
-            _ => panic!(),
-        }
+        let mut cache = HashMap::new();
+        Self::cached_kth_grammar(n, k, &mut cache)
     }
 }
 
