aaeb176c8d6514bf0c6fc7bcc36f24c56e0dfabf
duan <wddwyss@gmail.com>
2020-12-15 23:33:48 +0900

    optimal

--- number-of-islands/src/lib.rs
+++ number-of-islands/src/lib.rs
@@ -1,8 +1,6 @@
-use std::collections::HashSet;
-
 pub struct Solution;
 impl Solution {
-    pub fn num_islands(grid: Vec<Vec<char>>) -> i32 {
+    pub fn num_islands(mut grid: Vec<Vec<char>>) -> i32 {
         let m = grid.len();
         if m == 0 {
             return 0;
@@ -12,11 +10,11 @@ impl Solution {
             return 0;
         }
 
-        let mut visited = HashSet::new();
         let mut res = 0;
         for i in 0..m {
             for j in 0..n {
-                if Self::dfs(&grid, i, j, &mut visited) {
+                if grid[i][j] == '1' {
+                    Self::dfs(&mut grid, i, j);
                     res += 1;
                 }
             }
@@ -24,32 +22,23 @@ impl Solution {
         res
     }
 
-    fn dfs(
-        grid: &Vec<Vec<char>>,
-        i: usize,
-        j: usize,
-        visited: &mut HashSet<(usize, usize)>,
-    ) -> bool {
-        if visited.contains(&(i, j)) {
-            return false;
+    fn dfs(grid: &mut Vec<Vec<char>>, i: usize, j: usize) {
+        if grid[i][j] == '0' {
+            return;
         }
-        visited.insert((i, j));
-        if grid[i][j] == '1' {
-            if i < grid.len() - 1 {
-                Self::dfs(&grid, i + 1, j, visited);
-            }
-            if i > 0 {
-                Self::dfs(&grid, i - 1, j, visited);
-            }
-            if j < grid[0].len() - 1 {
-                Self::dfs(&grid, i, j + 1, visited);
-            }
-            if j > 0 {
-                Self::dfs(&grid, i, j - 1, visited);
-            }
-            return true;
-        } else {
-            return false;
+
+        grid[i][j] = '0';
+        if i < grid.len() - 1 {
+            Self::dfs(grid, i + 1, j);
+        }
+        if i > 0 {
+            Self::dfs(grid, i - 1, j);
+        }
+        if j < grid[0].len() - 1 {
+            Self::dfs(grid, i, j + 1);
+        }
+        if j > 0 {
+            Self::dfs(grid, i, j - 1);
         }
     }
 }
