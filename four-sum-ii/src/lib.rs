eb52af86da4f2013f6dee0b08730963d58b66f4c
duan <wddwyss@gmail.com>
2020-12-17 20:25:15 +0900

    optimal

--- four-sum-ii/src/lib.rs
+++ four-sum-ii/src/lib.rs
@@ -1,15 +1,24 @@
+use std::collections::HashMap;
+
 pub struct Solution;
 impl Solution {
     pub fn four_sum_count(a: Vec<i32>, b: Vec<i32>, c: Vec<i32>, d: Vec<i32>) -> i32 {
         let mut res = 0;
-        for av in a.iter() {
-            for bv in b.iter() {
-                for cv in c.iter() {
-                    for dv in d.iter() {
-                        if av + bv + cv + dv == 0 {
-                            res += 1;
-                        }
-                    }
+
+        let ab_sum = {
+            let mut sum = HashMap::new();
+            for i in &a {
+                for j in &b {
+                    *sum.entry(i + j).or_insert(0) += 1;
+                }
+            }
+            sum
+        };
+        for i in &c {
+            for j in &d {
+                if let Some(v) = ab_sum.get(&(0 - i - j)) {
+                    // TODO
+                    res += v;
                 }
             }
         }
