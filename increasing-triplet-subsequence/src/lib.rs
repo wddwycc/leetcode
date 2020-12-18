72aa0e056c3557a97d321e03dca76363f3e8bb08
duan <wddwyss@gmail.com>
2020-12-18 23:49:10 +0900

    optimal

--- increasing-triplet-subsequence/src/lib.rs
+++ increasing-triplet-subsequence/src/lib.rs
@@ -1,38 +1,15 @@
+use std::i32::MAX;
 pub struct Solution;
 impl Solution {
     pub fn increasing_triplet(nums: Vec<i32>) -> bool {
-        Self::helper(&nums)
-    }
-
-    pub fn helper(nums: &[i32]) -> bool {
-        let mut stack = vec![];
-        for (idx, n) in nums.iter().enumerate() {
-            match stack.len() {
-                0 => stack.push(n),
-                1 => {
-                    let prev = stack[0];
-                    if n > prev {
-                        stack.push(n)
-                    } else {
-                        stack.pop();
-                        stack.push(n);
-                    }
-                }
-                2 => {
-                    if n > stack[1] {
-                        return true;
-                    }
-                    if n > stack[0] && n < stack[1] {
-                        stack.pop();
-                        stack.push(n);
-                    }
-                    if n < stack[0] {
-                        if Self::helper(&nums[idx..]) {
-                            return true;
-                        }
-                    }
-                }
-                _ => panic!(),
+        let (mut s, mut l) = (MAX, MAX);
+        for n in nums {
+            if n < s {
+                s = n;
+            } else if n > s && n < l {
+                l = n;
+            } else if n > s && n > l {
+                return true;
             }
         }
         false
