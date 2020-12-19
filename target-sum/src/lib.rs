5fe818f64d3fa94a548e85b579dd65124ae2db4c
duan <wddwyss@gmail.com>
2020-12-19 14:49:44 +0900

    mem opt

--- target-sum/src/lib.rs
+++ target-sum/src/lib.rs
@@ -1,15 +1,18 @@
 pub struct Solution;
 impl Solution {
-    pub fn find_target_sum_ways(mut nums: Vec<i32>, s: i32) -> i32 {
-        // Brute force
-        if let Some(num) = nums.pop() {
-            return Self::find_target_sum_ways(nums.clone(), s - num)
-                + Self::find_target_sum_ways(nums, s + num);
+    pub fn find_target_sum_ways(nums: Vec<i32>, s: i32) -> i32 {
+        Self::helper(&nums, s)
+    }
+
+    pub fn helper(nums: &[i32], s: i32) -> i32 {
+        if let Some(num) = nums.last() {
+            Self::helper(&nums[0..(nums.len() - 1)], s - num)
+                + Self::helper(&nums[0..(nums.len() - 1)], s + num)
         } else {
             if s == 0 {
-                return 1;
+                1
             } else {
-                return 0;
+                0
             }
         }
     }
