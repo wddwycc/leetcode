fabd14fd66d4820c1897b8614561e3ee5dee19cf
duan <wddwyss@gmail.com>
2021-02-04 23:43:48 +0900

    bottom-up dp

--- partition-equal-subset-sum/src/lib.rs
+++ partition-equal-subset-sum/src/lib.rs
@@ -1,48 +1,57 @@
-use std::collections::HashMap;
-
 pub struct Solution;
 impl Solution {
     pub fn can_partition(nums: Vec<i32>) -> bool {
         let target: i32 = nums.iter().sum();
-        if target % 2 != 0 { return false }
+        if target % 2 != 0 {
+            return false;
+        }
         let target = target / 2;
 
         // Trasforms the question into:
         // Is it possible for subset of nums to sum up to target
-        
+
         // If enumerate all subsets, time would be O(2 ** n). as n can reach 200, would cause time exceed.
-        
-        // Try use top-down dp
-        // The state to memorize is:
-        // for nums[i..], whether there exists a subset sums up to j
-        // represented as HashMap<(i, j), bool>
-        let mut cache = HashMap::new();
-        Self::dfs(&nums, 0, target, &mut cache)
-    }
-    
-    pub fn dfs(nums: &[i32], index: usize, sum: i32, cache: &mut HashMap<(usize, i32), bool>) -> bool {
-        if let Some(cached) = cache.get(&(index, sum)) {
-            return *cached;
+
+        // Try use bottom-up dp
+        // Given function f(i, v) -> bool, represents whether first i + 1 elements can sum up to v
+        // for i == 0, f(0, v) = nums[0] == v (bottom case)
+        // for i > 0,  f(i, v) = f(i - 1, v - nums[i]) || f(i - 1, v)
+        // so we can have our 2d dp
+        let mut dp = vec![vec![false; target as usize]; nums.len()];
+        for i in 0..nums.len() {
+            if i == 0 {
+                for v in 1..=target {
+                    dp[0][v as usize - 1] = nums[0] == v;
+                }
+            } else {
+                for v in 1..=target {
+                    dp[i][v as usize - 1] = {
+                        let minus = v - nums[i];
+                        if minus < 0 {
+                            dp[i - 1][v as usize - 1]
+                        } else if minus == 0 {
+                            true
+                        } else {
+                            dp[i - 1][v as usize - 1] || dp[i - 1][minus as usize - 1]
+                        }
+                    }
+                }
+            }
+            if dp[i][target as usize - 1] {
+                return true;
+            }
         }
-        if sum < 0 {
-            return false;
-        }
-        if sum == 0 {
-            return true;
-        }
-        if index == nums.len() {
-            return false;
-        }
-        let res = Self::dfs(nums, index + 1, sum, cache) || Self::dfs(nums, index + 1, sum - nums[index], cache);
-        cache.insert((index, sum), res);
-        res
+        false
     }
 }
 
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
     fn it_works() {
-        assert_eq!(2 + 2, 4);
+        assert_eq!(Solution::can_partition(vec![1, 5, 11, 5]), true);
+        assert_eq!(Solution::can_partition(vec![2, 2, 3, 5]), false);
     }
 }
