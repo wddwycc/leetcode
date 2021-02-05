508c0cf965de9c26d987935288a997a093f24fcb
duan <wddwyss@gmail.com>
2021-02-05 22:52:56 +0900

    opt calc

--- minimum-path-sum/src/lib.rs
+++ minimum-path-sum/src/lib.rs
@@ -1,8 +1,8 @@
 pub struct Solution;
 impl Solution {
     pub fn min_path_sum(mut grid: Vec<Vec<i32>>) -> i32 {
-        let m = grid.len();
-        let n = grid[0].len();
+        let m = grid.len() - 1;
+        let n = grid[0].len() - 1;
 
         // give function f(i, j) represents: for grid[i][j], min sum to reach right-botom
         // if i < m && j < n, f(i, j) = grid[i][j] + min { f(i + 1, j), f(i, j + 1) }
@@ -10,14 +10,14 @@ impl Solution {
         // if          j < n, f(i, j) = grid[i][j] + f(i, j + 1)
         // else             , f(i, j) = grid[i][j]  // bottom-case
 
-        for i in (0..m).rev() {
-            for j in (0..n).rev() {
+        for i in (0..=m).rev() {
+            for j in (0..=n).rev() {
                 grid[i][j] = grid[i][j] + {
-                    if i + 1 == m && j + 1 == n {
+                    if i == m && j == n {
                         0
-                    } else if i + 1 == m {
+                    } else if i == m {
                         grid[i][j + 1]
-                    } else if j + 1 == n {
+                    } else if j == n {
                         grid[i + 1][j]
                     } else {
                         std::cmp::min(grid[i + 1][j], grid[i][j + 1])
@@ -28,7 +28,6 @@ impl Solution {
         grid[0][0]
     }
 }
-
 #[cfg(test)]
 mod tests {
     #[test]
