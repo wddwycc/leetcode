6b1dac747ae753059869585123788f347625dc10
duan <wddwyss@gmail.com>
2021-02-21 23:20:48 +0900

    opt with onepass

--- search-a-2d-matrix-ii/src/lib.rs
+++ search-a-2d-matrix-ii/src/lib.rs
@@ -1,42 +1,22 @@
-use std::collections::HashSet;
-
 pub struct Solution;
 impl Solution {
     pub fn search_matrix(matrix: Vec<Vec<i32>>, target: i32) -> bool {
-        // just search with DFS, time should be O(mn), space should be O(m + n)
         let m = matrix.len() - 1;
         let n = matrix[0].len() - 1;
-        Self::dfs(&matrix, (m, n), (0, 0), target, &mut HashSet::new())
-    }
 
-    fn dfs(
-        matrix: &[Vec<i32>],
-        bounds: (usize, usize),
-        cur: (usize, usize),
-        target: i32,
-        visited: &mut HashSet<(usize, usize)>,
-    ) -> bool {
-        if visited.contains(&cur) {
-            return false;
+        let mut x = m;
+        let mut y = 0;
+        loop {
+            if matrix[x][y] == target {
+                return true;
+            } else if matrix[x][y] > target && x > 0 {
+                x -= 1;
+            } else if matrix[x][y] < target && y < n {
+                y += 1;
+            } else {
+                return false;
+            }
         }
-        visited.insert(cur);
-
-        let (m, n) = bounds;
-        let (x, y) = cur;
-        let val = matrix[x][y];
-        if val == target {
-            return true;
-        }
-        if val > target {
-            return false;
-        }
-        if x < m && Self::dfs(matrix, bounds, (x + 1, y), target, visited) {
-            return true;
-        }
-        if y < n && Self::dfs(matrix, bounds, (x, y + 1), target, visited) {
-            return true;
-        }
-        return false;
     }
 }
 
