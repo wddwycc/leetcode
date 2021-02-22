82f4141fd726240ef983798c3cf19d546257c652
duan <wddwyss@gmail.com>
2021-02-22 23:41:20 +0900

    opt

--- longest-word-in-dictionary-through-deleting/src/lib.rs
+++ longest-word-in-dictionary-through-deleting/src/lib.rs
@@ -1,4 +1,4 @@
-use std::cmp::Reverse;
+use std::{cmp::Reverse, str::Chars};
 
 pub struct Solution;
 impl Solution {
@@ -7,29 +7,29 @@ impl Solution {
     // Time => O(nk + nlogn), space => O(1)
     pub fn find_longest_word(s: String, mut d: Vec<String>) -> String {
         d.sort_by(|a, b| (Reverse(a.len()), a).cmp(&(Reverse(b.len()), b)));
-
-        let s: Vec<char> = s.chars().collect();
         for word in d {
-            if Self::check_viable(&word.chars().collect::<Vec<char>>(), &s) {
+            if Self::check_viable(word.chars(), s.chars()) {
                 return word;
             }
         }
         return "".to_owned();
     }
 
-    fn check_viable(word: &[char], src: &[char]) -> bool {
-        let m = word.len();
-        let n = src.len();
-
-        let mut i = 0;
-        let mut j = 0;
-        while i < m && j < n {
-            if word[i] == src[j] {
-                i += 1;
+    fn check_viable(word: Chars, mut src: Chars) -> bool {
+        let mut word = word.peekable();
+        while let Some(c) = word.peek() {
+            loop {
+                let sc = match src.next() {
+                    Some(a) => a,
+                    None => return false,
+                };
+                if &sc == c {
+                    word.next();
+                    break;
+                }
             }
-            j += 1;
         }
-        i == m
+        true
     }
 }
 
