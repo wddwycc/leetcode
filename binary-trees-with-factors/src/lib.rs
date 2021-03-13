e30734e4c2ab9428428a4c90ebd385e34d795fd9
duan <wddwyss@gmail.com>
2021-03-13 23:43:13 +0900

    solved

--- binary-trees-with-factors/src/lib.rs
+++ binary-trees-with-factors/src/lib.rs
@@ -2,25 +2,25 @@ use std::collections::HashMap;
 
 pub struct Solution;
 impl Solution {
-    pub fn num_factored_binary_trees(mut arr: Vec<i32>) -> i32 {
+    pub fn num_factored_binary_trees(arr: Vec<i32>) -> i32 {
+        let mut arr: Vec<i64> = arr.into_iter().map(|a| a as i64).collect();
         arr.sort();
 
-        let mut products: HashMap<i32, Vec<(i32, i32)>> = HashMap::new();
+        let mut products: HashMap<i64, Vec<(i64, i64)>> = HashMap::new();
         let mut dist = HashMap::new();
+        let modulo = 10_i64.pow(9 as u32) + 7;
 
         for i in 0..arr.len() {
             let mut count = 1;
             if let Some(pairs) = products.get(&arr[i]) {
-                count += pairs
-                    .iter()
-                    .map(|(l, r)| {
-                        let mut res = dist.get(l).unwrap() * dist.get(r).unwrap();
-                        if l != r {
-                            res *= 2;
-                        }
-                        res
-                    })
-                    .sum::<i32>();
+                for pair in pairs {
+                    let (l, r) = pair;
+                    let mut res = dist.get(l).unwrap() * dist.get(r).unwrap();
+                    if l != r {
+                        res *= 2;
+                    }
+                    count += res;
+                }
             }
             for j in 0..=i {
                 if let Some(k) = arr[i].checked_mul(arr[j]) {
@@ -29,8 +29,8 @@ impl Solution {
             }
             dist.insert(arr[i], count);
         }
-        println!("{:?}", dist);
-        dist.values().sum()
+
+        (dist.values().sum::<i64>() % modulo) as i32
     }
 }
 
