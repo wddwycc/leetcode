805a3a1312990e26a62f37ba9bb271812ab83209
duan <wddwyss@gmail.com>
2021-03-20 12:40:57 +0900

    opt

--- keys-and-rooms/src/lib.rs
+++ keys-and-rooms/src/lib.rs
@@ -2,26 +2,20 @@ use std::collections::HashSet;
 
 pub struct Solution;
 impl Solution {
+    // DFS => time: O(n), space: O(n)
     pub fn can_visit_all_rooms(mut rooms: Vec<Vec<i32>>) -> bool {
-        // DFS, O(n)
         let mut visited = HashSet::new();
         visited.insert(0);
         let mut stack = vec![];
         stack.append(&mut rooms[0]);
         while let Some(idx) = stack.pop() {
             if visited.contains(&idx) {
-                break;
+                continue;
             }
             visited.insert(idx);
             stack.append(&mut rooms[idx as usize]);
         }
-        // validate result, O(n)
-        for n in 0..rooms.len() {
-            if !visited.contains(&(n as i32)) {
-                return false;
-            }
-        }
-        true
+        visited.len() == rooms.len()
     }
 }
 
