b25040eb52454ec87f9c3316dd37b01735001e7c
duan <wddwyss@gmail.com>
2021-03-21 23:22:55 +0900

    bottom-up DP

--- coin-change/src/lib.rs
+++ coin-change/src/lib.rs
@@ -1,36 +1,27 @@
-use std::collections::HashMap;
-
 pub struct Solution;
 impl Solution {
     pub fn coin_change(coins: Vec<i32>, amount: i32) -> i32 {
         // give f(s) = k, represents to sum up to s, min coin nums is k
+        // f(0) = 0
+        // f(s) = min { for coin in coins:  f(s - coin) } + 1
 
-        // let's try top-down DP
-        // memoization: HashMap<s, k>
-        let mut cache = HashMap::new();
-        Self::dfs(&coins, amount, &mut cache)
-            .map(|a| a as i32)
-            .unwrap_or(-1)
-    }
-
-    fn dfs(coins: &[i32], target: i32, cache: &mut HashMap<i32, Option<usize>>) -> Option<usize> {
-        if let Some(cached) = cache.get(&target) {
-            return *cached;
+        // let's try bottom-up dp
+        let mut res: Vec<Option<i32>> = vec![Some(0)];
+        for i in 1..=amount {
+            let v = coins
+                .iter()
+                .filter_map(|coin| {
+                    if i >= *coin {
+                        res[(i - *coin) as usize]
+                    } else {
+                        None
+                    }
+                })
+                .min()
+                .map(|a| a + 1);
+            res.push(v);
         }
-
-        if target == 0 {
-            return Some(0);
-        }
-        if target < 0 {
-            return None;
-        }
-        let res = coins
-            .iter()
-            .filter_map(|c| Self::dfs(coins, target - c, cache))
-            .min()
-            .map(|a| a + 1);
-        cache.insert(target, res);
-        res
+        res.last().and_then(|a| *a).unwrap_or(-1)
     }
 }
 
