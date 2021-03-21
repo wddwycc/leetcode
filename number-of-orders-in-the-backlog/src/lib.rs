38c2081c6e03e7eb5434853cfe97597838d5629d
duan <wddwyss@gmail.com>
2021-03-21 13:11:37 +0900

    opt

--- number-of-orders-in-the-backlog/src/lib.rs
+++ number-of-orders-in-the-backlog/src/lib.rs
@@ -25,31 +25,27 @@ pub struct Solution;
 impl Solution {
     pub fn get_number_of_backlog_orders(orders: Vec<Vec<i32>>) -> i32 {
         let mut buy_heap: BinaryHeap<Orders> = BinaryHeap::new();
-        let mut sell_heap: BinaryHeap<Reverse<Orders>> = BinaryHeap::new(); // Reverse<i32>
+        let mut sell_heap: BinaryHeap<Reverse<Orders>> = BinaryHeap::new(); // Reverse<Orders>
 
         for order in orders {
             let price = order[0];
             let mut amount = order[1];
             let is_buy = order[2] == 0;
             if is_buy {
-                loop {
-                    if let Some(sell_orders) = sell_heap.pop() {
-                        if price >= sell_orders.0.price {
-                            if amount >= sell_orders.0.amount {
-                                amount -= sell_orders.0.amount;
-                            } else {
-                                sell_heap.push(Reverse(Orders {
-                                    price: sell_orders.0.price,
-                                    amount: sell_orders.0.amount - amount,
-                                }));
-                                amount = 0;
-                                break;
-                            }
+                while let Some(sell_orders) = sell_heap.pop() {
+                    if price >= sell_orders.0.price {
+                        if amount >= sell_orders.0.amount {
+                            amount -= sell_orders.0.amount;
                         } else {
-                            sell_heap.push(sell_orders);
+                            sell_heap.push(Reverse(Orders {
+                                price: sell_orders.0.price,
+                                amount: sell_orders.0.amount - amount,
+                            }));
+                            amount = 0;
                             break;
                         }
                     } else {
+                        sell_heap.push(sell_orders);
                         break;
                     }
                 }
@@ -57,24 +53,20 @@ impl Solution {
                     buy_heap.push(Orders { price, amount });
                 }
             } else {
-                loop {
-                    if let Some(buy_orders) = buy_heap.pop() {
-                        if price <= buy_orders.price {
-                            if amount >= buy_orders.amount {
-                                amount -= buy_orders.amount;
-                            } else {
-                                buy_heap.push(Orders {
-                                    price: buy_orders.price,
-                                    amount: buy_orders.amount - amount,
-                                });
-                                amount = 0;
-                                break;
-                            }
+                while let Some(buy_orders) = buy_heap.pop() {
+                    if price <= buy_orders.price {
+                        if amount >= buy_orders.amount {
+                            amount -= buy_orders.amount;
                         } else {
-                            buy_heap.push(buy_orders);
+                            buy_heap.push(Orders {
+                                price: buy_orders.price,
+                                amount: buy_orders.amount - amount,
+                            });
+                            amount = 0;
                             break;
                         }
                     } else {
+                        buy_heap.push(buy_orders);
                         break;
                     }
                 }
@@ -82,9 +74,6 @@ impl Solution {
                     sell_heap.push(Reverse(Orders { price, amount }));
                 }
             }
-            println!("round:");
-            println!("buy: {:?}", buy_heap);
-            println!("sell: {:?}", sell_heap);
         }
         let modolo = (10 as i64).pow(9) + 7;
         let res = buy_heap.iter().map(|a| a.amount as i64).sum::<i64>()
