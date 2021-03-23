f4abb65f9aaa9ca5a5fec85b4dbc3b39ac5a287f
duan <wddwyss@gmail.com>
2021-03-24 00:33:04 +0900

    continue opt

--- three-sum/src/main.rs
+++ three-sum/src/main.rs
@@ -3,18 +3,18 @@ use std::collections::{HashMap, HashSet};
 pub struct Solution {}
 impl Solution {
     // time: O(n), space: O(n)
-    pub fn two_sum(nums: &[i32], sum: i32) -> Vec<Vec<i32>> {
-        let mut result = vec![];
+    pub fn two_sum(nums: &[i32], sum: i32) -> Vec<(i32, i32)> {
+        let mut res = vec![];
         let mut remainders = HashMap::new();
         for i in 0..nums.len() {
             let val = nums[i];
             if let Some(&prev_i) = remainders.get(&val) {
-                result.push(vec![nums[prev_i], val]);
+                res.push((nums[prev_i], val));
             } else {
                 remainders.insert(sum - val, i);
             }
         }
-        result
+        res
     }
 
     // time: O(n ** 2), space: O(n)
@@ -25,23 +25,21 @@ impl Solution {
         }
 
         let mut visited = HashSet::new();
-        let mut result = vec![];
+        let mut res = vec![];
 
         nums.sort();
         for idx in 0..(len - 2) {
             let val = nums[idx];
             let remainder = 0 - nums[idx];
-            let res = Solution::two_sum(&nums[(idx + 1)..], remainder);
-            for item in res {
-                let key = (val, item[0], item[1]);
+            for item in Solution::two_sum(&nums[(idx + 1)..], remainder) {
+                let key = (val, item.0, item.1);
                 if !visited.contains(&key) {
-                    result.push(vec![val, item[0], item[1]]);
+                    res.push(vec![val, item.0, item.1]);
                     visited.insert(key);
                 }
             }
         }
-
-        result
+        res
     }
 }
 
