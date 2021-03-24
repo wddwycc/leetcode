89218db63afd887bb5afaa2da87ceb083c836ced
duan <wddwyss@gmail.com>
2021-03-24 23:09:19 +0900

    optimal

--- advantage-shuffle/src/lib.rs
+++ advantage-shuffle/src/lib.rs
@@ -1,42 +1,29 @@
 pub struct Solution;
 impl Solution {
-    // time: O(n^2)
+    // time: O(nlogn)
     pub fn advantage_count(mut a: Vec<i32>, b: Vec<i32>) -> Vec<i32> {
         let n = a.len();
         a.sort();
         let mut b: Vec<(usize, i32)> = b.into_iter().enumerate().collect();
         b.sort_by(|a, b| a.1.cmp(&b.1));
-        let (_, offset) = Self::best_solution(&a, &b.iter().map(|a| a.1).collect::<Vec<i32>>(), 0);
-        for i in 0..offset {
-            let fst = a.remove(0);
-            a.insert(n - i - 1, fst);
-        }
 
         let mut res = vec![0; n];
-        for i in 0..n {
-            res[b[i].0] = a[i];
+        let mut slow = 0;
+        let mut fast = n - 1;
+        while let Some((opponent_idx, opponent)) = b.pop() {
+            res[opponent_idx] = {
+                if opponent >= a[fast] {
+                    let v = a[slow];
+                    slow += 1;
+                    v
+                } else {
+                    let v = a[fast];
+                    fast -= 1;
+                    v
+                }
+            }
         }
         res
-    }
-
-    // find best offset to apply to reach max advantage for two sorted array
-    fn best_solution(a: &[i32], b: &[i32], offset: usize) -> (usize, usize) {
-        let n = a.len();
-        if n == 1 {
-            if a[0] > b[0] {
-                return (1, offset);
-            } else {
-                return (0, offset);
-            }
-        }
-        let res: usize = (0..n).map(|i| if a[i] > b[i] { 1 } else { 0 }).sum();
-        if a[0] < b[n - 1] {
-            let new_res = Self::best_solution(&a[1..], &b[..(n - 1)], offset + 1);
-            if new_res.0 > res {
-                return new_res;
-            }
-        }
-        (res, offset)
     }
 }
 
