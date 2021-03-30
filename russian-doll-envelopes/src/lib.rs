6a6e09d84986bd4b5ef25332a5d109a9eb2de6d7
duan <wddwyss@gmail.com>
2021-03-30 22:28:35 +0900

    opt

--- russian-doll-envelopes/src/lib.rs
+++ russian-doll-envelopes/src/lib.rs
@@ -1,32 +1,17 @@
 pub struct Solution;
 impl Solution {
     pub fn max_envelopes(mut envelopes: Vec<Vec<i32>>) -> i32 {
-        envelopes.sort();
-        let heights = {
-            let mut res = vec![];
-            let mut width = envelopes[0][0];
-            let mut heights = vec![envelopes[0][1]];
-            for envelope in &envelopes[1..] {
-                if envelope[0] == width {
-                    heights.push(envelope[1]);
-                } else {
-                    heights.reverse();
-                    res.append(&mut heights);
-                    width = envelope[0];
-                    heights = vec![envelope[1]];
-                }
-            }
-            heights.reverse();
-            res.append(&mut heights);
-            res
-        };
-        // TODO: improve sorting
-        Self::longest_increasing_subsequence(&heights)
+        envelopes.sort_by(|x, y| match x[0].cmp(&y[0]) {
+            std::cmp::Ordering::Equal => y[1].cmp(&x[1]),
+            a => a,
+        });
+        let heights: Vec<i32> = envelopes.into_iter().map(|a| a[1]).collect();
+        Self::longest_increasing_subsequence(&heights) as i32
     }
 
-    fn longest_increasing_subsequence(nums: &[i32]) -> i32 {
+    fn longest_increasing_subsequence(nums: &[i32]) -> usize {
         if nums.len() <= 1 {
-            return nums.len() as i32;
+            return nums.len();
         }
         let mut res = vec![];
         res.push(nums[0]);
@@ -42,7 +27,7 @@ impl Solution {
                 _ => (),
             }
         }
-        res.len() as i32
+        res.len()
     }
 }
 
