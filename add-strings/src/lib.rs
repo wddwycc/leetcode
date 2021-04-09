74d604aba57c0c7922a7712bef33dbd4887f3531
duan <wddwyss@gmail.com>
2021-04-10 00:04:38 +0900

    simplify impl

--- add-strings/src/lib.rs
+++ add-strings/src/lib.rs
@@ -9,33 +9,20 @@ impl Solution {
         let mut i = 0;
         let mut j = 0;
         let mut digits = vec![];
-        let mut need_increment = false;
-        loop {
-            if m - 1 >= i || n - 1 >= j {
-                let mut sum = 0;
-                if m - 1 >= i {
-                    sum += num1[m - 1 - i] - b'0';
-                    i += 1;
-                }
-                if n - 1 >= j {
-                    sum += num2[n - 1 - j] - b'0';
-                    j += 1;
-                }
-                if need_increment {
-                    sum += 1;
-                    need_increment = false;
-                }
-                if sum >= 10 {
-                    sum -= 10;
-                    need_increment = true;
-                }
-                digits.push(sum + b'0');
-            } else if need_increment {
-                digits.push(b'1');
-                break;
-            } else {
-                break;
+        let mut carry = 0;
+        while m - 1 >= i || n - 1 >= j || carry > 0 {
+            let mut sum = 0;
+            if m - 1 >= i {
+                sum += num1[m - 1 - i] - b'0';
+                i += 1;
             }
+            if n - 1 >= j {
+                sum += num2[n - 1 - j] - b'0';
+                j += 1;
+            }
+            sum += carry;
+            carry = sum / 10;
+            digits.push(sum % 10 + b'0');
         }
         digits.reverse();
         std::str::from_utf8(&digits).unwrap().to_owned()
