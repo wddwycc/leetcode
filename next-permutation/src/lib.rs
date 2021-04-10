b1430782b8ac19e13c23e172bc77536222d53bf2
duan <wddwyss@gmail.com>
2021-04-10 12:44:30 +0900

    opt

--- next-permutation/src/lib.rs
+++ next-permutation/src/lib.rs
@@ -1,39 +1,27 @@
 pub struct Solution;
 impl Solution {
     pub fn next_permutation(nums: &mut Vec<i32>) {
-        let mut cur = nums.len() - 1;
         let mut heap = std::collections::BinaryHeap::new();
-        loop {
-            if let Some(&(max, _)) = heap.peek() {
-                // find the min previous element greater than nums[cur]
-                // if exist, swap then sort.
-                // else, next iteration
-                if max > nums[cur] {
-                    let mut m = heap.pop().unwrap();
-                    while let Some(v) = heap.pop() {
-                        if v.0 > nums[cur] {
-                            m = v;
-                        } else {
-                            break;
-                        }
-                    }
-                    nums.swap(cur, m.1);
-                    nums[(cur + 1)..].sort();
-                    return;
-                } else {
-                    heap.push((nums[cur], cur));
+        for i in (0..nums.len()).rev() {
+            let max = match heap.peek() {
+                Some(&(a, _)) => a,
+                None => {
+                    heap.push((nums[i], i));
+                    continue;
                 }
+            };
+            // find the min previous element greater than nums[cur]
+            // if exist, swap, sort, return
+            if max > nums[i] {
+                let (_, to_swap) = heap.into_iter().filter(|a| a.0 > nums[i]).min().unwrap();
+                nums.swap(i, to_swap);
+                nums[(i + 1)..].sort();
+                return;
             } else {
-                heap.push((nums[cur], cur));
-            }
-
-            if cur == 0 {
-                nums.sort();
-                break;
-            } else {
-                cur -= 1;
+                heap.push((nums[i], i));
             }
         }
+        nums.sort();
     }
 }
 
