23e68f8c2b88b0750b0c861acbd366248452edee
duan <wddwyss@gmail.com>
2021-04-13 22:50:10 +0900

    opt

--- flatten-nested-list-iterator/src/lib.rs
+++ flatten-nested-list-iterator/src/lib.rs
@@ -1,61 +1,45 @@
-// #[derive(Debug, PartialEq, Eq)]
+#[derive(Debug, PartialEq, Eq)]
 pub enum NestedInteger {
     Int(i32),
     List(Vec<NestedInteger>),
 }
 
-impl NestedInteger {
-    fn flatten(self) -> Vec<i32> {
-        match self {
-            Self::Int(a) => vec![a],
-            Self::List(list) => list.into_iter().map(|a| a.flatten()).flatten().collect(),
-        }
-    }
-}
-
 struct NestedIterator {
-    cur: usize,
-    data: Vec<i32>,
+    stack: Vec<NestedInteger>,
 }
 
-/**
- * `&self` means the method takes an immutable reference.
- * If you need a mutable reference, change it to `&mut self` instead.
- */
 impl NestedIterator {
-    fn new(nestedList: Vec<NestedInteger>) -> Self {
-        Self {
-            cur: 0,
-            data: NestedInteger::List(nestedList).flatten(),
+    fn new(mut nestedList: Vec<NestedInteger>) -> Self {
+        nestedList.reverse();
+        Self { stack: nestedList }
+    }
+
+    fn consume(&mut self) -> Option<i32> {
+        if let Some(nested_i) = self.stack.pop() {
+            match nested_i {
+                NestedInteger::Int(a) => return Some(a),
+                NestedInteger::List(mut list) => {
+                    list.reverse();
+                    for a in list {
+                        self.stack.push(a);
+                    }
+                    return self.consume();
+                }
+            }
         }
+        None
     }
 
     fn next(&mut self) -> i32 {
-        if self.has_next() {
-            let data = self.data[self.cur];
-            self.cur += 1;
-            return data;
+        self.consume().unwrap_or(-1)
+    }
+
+    fn has_next(&mut self) -> bool {
+        if let Some(a) = self.consume() {
+            self.stack.push(NestedInteger::Int(a));
+            return true;
         } else {
-            return -1;
+            return false;
         }
-    }
-
-    fn has_next(&self) -> bool {
-        self.cur < self.data.len()
-    }
-}
-
-/**
- * Your NestedIterator object will be instantiated and called as such:
- * let obj = NestedIterator::new(nestedList);
- * let ret_1: i32 = obj.next();
- * let ret_2: bool = obj.has_next();
- */
-
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
     }
 }
