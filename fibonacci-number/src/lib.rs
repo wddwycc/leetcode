90adce1bcb0de7d820cb6aeef7a35afa2f1876c9
duan <wddwyss@gmail.com>
2021-04-15 21:15:31 +0900

    fibonacci

--- fibonacci-number/src/lib.rs
+++ fibonacci-number/src/lib.rs
@@ -1,33 +1,19 @@
 pub struct Solution {}
 impl Solution {
     pub fn fib(n: i32) -> i32 {
-        fn calc_with_cache(n: i32, cache: &mut Vec<Option<i32>>) -> i32 {
-            match n {
-                0 => 0,
-                1 => 1,
-                _ => {
-                    let v1 = match cache[n as usize - 1] {
-                        Some(a) => a,
-                        None => {
-                            let rv = calc_with_cache(n - 1, cache);
-                            cache[n as usize - 1] = Some(rv);
-                            rv
-                        }
-                    };
-                    let v2 = match cache[n as usize - 2] {
-                        Some(a) => a,
-                        None => {
-                            let rv = calc_with_cache(n - 2, cache);
-                            cache[n as usize - 2] = Some(rv);
-                            rv
-                        }
-                    };
-                    v1 + v2
-                },
-            }    
+        if n == 0 {
+            return 0;
         }
-        let mut cache: Vec<Option<i32>> = vec![None; n as usize];
-        calc_with_cache(n, &mut cache)
+        if n == 1 {
+            return 1;
+        }
+        let mut dp = (0, 1);
+        for _ in 2..=(n as usize) {
+            let res = dp.0 + dp.1;
+            dp.0 = dp.1;
+            dp.1 = res;
+        }
+        dp.1
     }
 }
 
