737fcba7631692cc0f43772f698a06a64de80f43
duan <wddwyss@gmail.com>
2021-04-15 21:42:23 +0900

    continue opt

--- minimum-swaps-to-group-all-1s-together/src/lib.rs
+++ minimum-swaps-to-group-all-1s-together/src/lib.rs
@@ -9,20 +9,18 @@ impl Solution {
         }
 
         let mut first_is_zero = data[0] == 0;
-        let mut prev = (0..k).filter(|&i| data[i] == 0).count() as i32;
-        let mut min = prev;
+        let mut steps = (0..k).filter(|&i| data[i] == 0).count() as i32;
+        let mut min = steps;
 
         for i in 1..=(n - k) {
-            let mut next_v = prev;
             if first_is_zero {
-                next_v -= 1;
+                steps -= 1;
             }
             if data[i + k - 1] == 0 {
-                next_v += 1;
+                steps += 1;
             }
             first_is_zero = data[i] == 0;
-            prev = next_v;
-            min = min.min(next_v);
+            min = min.min(steps);
         }
         min
     }
