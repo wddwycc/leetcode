91d77eec3be34262ae70022d366b29865366729d
duan <wddwyss@gmail.com>
2021-04-20 22:12:53 +0900

    Update desc

--- find-xor-sum-of-all-pairs-bitwise-and/src/lib.rs
+++ find-xor-sum-of-all-pairs-bitwise-and/src/lib.rs
@@ -1,7 +1,7 @@
 pub struct Solution;
 impl Solution {
     pub fn get_xor_sum(arr1: Vec<i32>, arr2: Vec<i32>) -> i32 {
-        // for every i-th bit:
+        // use arr1[0] to pair with all arr2, every i-th bit can be calculated separately:
         // a1&b1 ^ a1&b2 ^ a1&b3 .... ^ a1&bn
         // if a1 == 0, ans = 0
         // if a1 == 1, ans = b1 ^ b2 ... ^ bn, result depends on how many 1s ranged from { b1...bn }
