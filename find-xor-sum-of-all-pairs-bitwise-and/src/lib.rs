bc2ca1e6c71665af623f063df049ee50e0f1383c
duan <wddwyss@gmail.com>
2021-04-20 22:37:49 +0900

    .

--- find-xor-sum-of-all-pairs-bitwise-and/src/lib.rs
+++ find-xor-sum-of-all-pairs-bitwise-and/src/lib.rs
@@ -1,10 +1,8 @@
 pub struct Solution;
 impl Solution {
     pub fn get_xor_sum(arr1: Vec<i32>, arr2: Vec<i32>) -> i32 {
-        // if we know distributive property of bitwise:
+        // if we know distributive property of bitwise operator:
         // (a1^a2) & (b1^b2) = (a1&b1) ^ (a1&b2) ^ (a2&b1) ^ (a2&b2)
-        let l = arr1.into_iter().fold(0, |res, v| res ^ v);
-        let r = arr2.into_iter().fold(0, |res, v| res ^ v);
-        l & r
+        arr1.into_iter().fold(0, |a, b| a ^ b) & arr2.into_iter().fold(0, |a, b| a ^ b)
     }
 }
