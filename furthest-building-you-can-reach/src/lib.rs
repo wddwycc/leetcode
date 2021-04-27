924957995a492ce97888889af3c95c9f25e60420
duan <wddwyss@gmail.com>
2021-04-27 21:40:04 +0900

    fix solution

--- furthest-building-you-can-reach/src/lib.rs
+++ furthest-building-you-can-reach/src/lib.rs
@@ -1,49 +1,24 @@
-use std::collections::BinaryHeap;
+use std::{cmp::Reverse, collections::BinaryHeap};
 
 pub struct Solution;
 impl Solution {
-    pub fn furthest_building(heights: Vec<i32>, mut bricks: i32, mut ladders: i32) -> i32 {
-        let mut bricks_pq: BinaryHeap<(i32, usize)> = BinaryHeap::new();
-        let mut ladders_pq: BinaryHeap<(i32, usize)> = BinaryHeap::new();
-        let mut cur = 0;
-        while cur < heights.len() {
-            // Note: is last
-            if cur + 1 == heights.len() {
-                return cur as i32;
-            }
-            // Note: can move directly
-            let offset = heights[cur + 1] - heights[cur];
+    pub fn furthest_building(heights: Vec<i32>, mut bricks: i32, ladders: i32) -> i32 {
+        let mut pq = BinaryHeap::new();
+        for i in 1..heights.len() {
+            let offset = heights[i] - heights[i - 1];
             if offset <= 0 {
-                cur += 1;
                 continue;
             }
-            if bricks >= offset {
-                // Note: use bricks when possible
-                bricks -= offset;
-                bricks_pq.push((offset, cur));
-                cur += 1;
-            } else if ladders > 0 {
-                // Note: use ladder when possible
-                ladders -= 1;
-                ladders_pq.push((offset, cur));
-                cur += 1;
-            } else {
-                // Note: replace ladders internally
-                if let Some(&a) = ladders_pq.peek() {
-                    if let Some(&b) = bricks_pq.peek() {
-                        if a.0 < b.0 {
-                            bricks += b.0 - a.0;
-                            ladders_pq.pop();
-                            bricks_pq.pop();
-                            ladders_pq.push(b);
-                            bricks_pq.push(a);
-                            continue;
-                        }
-                    }
+            pq.push(Reverse(offset));
+            if pq.len() > ladders as usize {
+                let to_replace = pq.pop().unwrap().0;
+                if to_replace <= bricks {
+                    bricks -= to_replace;
+                } else {
+                    return i as i32 - 1;
                 }
-                return cur as i32;
             }
         }
-        panic!()
+        heights.len() as i32 - 1
     }
 }
