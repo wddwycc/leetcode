a5c2bf1006656d419fa2b304e17f697e2a12bb42
duan <wddwyss@gmail.com>
2021-04-28 22:00:00 +0900

    Simplify solution

--- unique-paths-ii/src/lib.rs
+++ unique-paths-ii/src/lib.rs
@@ -3,47 +3,23 @@ impl Solution {
     pub fn unique_paths_with_obstacles(obstacle_grid: Vec<Vec<i32>>) -> i32 {
         let m = obstacle_grid.len();
         let n = obstacle_grid[0].len();
-
-        if obstacle_grid[0][0] == 0 || obstacle_grid[m - 1][n - 1] == 0 {
+        if obstacle_grid[m - 1][n - 1] == 1 {
             return 0;
         }
 
-        let mut res = vec![vec![0; n]; m];
-        for i in 0..m {
-            for j in 0..n {
-                match (i, j) {
-                    (0, 0) => res[i][j] = 1,
-                    (0, j) => {
-                        if obstacle_grid[i][j - 1] == 0 {
-                            res[i][j] = res[i][j - 1];
-                        }
-                    }
-                    (i, 0) => {
-                        if obstacle_grid[i - 1][j] == 0 {
-                            res[i][j] = res[i - 1][j]
-                        }
-                    }
-                    (i, j) => {
-                        let mut val = 0;
-                        if obstacle_grid[i - 1][j] == 0 {
-                            val += res[i - 1][j];
-                        }
-                        if obstacle_grid[i][j - 1] == 0 {
-                            val += res[i][j - 1]
-                        }
-                        res[i][j] = val;
+        let mut dp = vec![0; n];
+        dp[n - 1] = 1;
+        for i in (0..m).rev() {
+            for j in (0..n).rev() {
+                if obstacle_grid[i][j] == 1 {
+                    dp[j] = 0;
+                } else {
+                    if j + 1 < n {
+                        dp[j] += dp[j + 1];
                     }
                 }
             }
         }
-        res[m - 1][n - 1]
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+        dp[0]
     }
 }
