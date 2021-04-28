13a22904907d8e58159ff300c58cbff86748fa78
duan <wddwyss@gmail.com>
2021-04-28 22:37:44 +0900

    .

---
+++ unique-paths-iii/src/lib.rs
@@ -0,0 +1,69 @@
+use std::collections::HashSet;
+
+pub struct Solution;
+impl Solution {
+    pub fn unique_paths_iii(grid: Vec<Vec<i32>>) -> i32 {
+        let m = grid.len();
+        let n = grid[0].len();
+        let cur = (|| {
+            for i in 0..m {
+                for j in 0..n {
+                    if grid[i][j] == 1 {
+                        return (i, j);
+                    }
+                }
+            }
+            panic!();
+        })();
+        let target = {
+            let mut res = 0;
+            for i in 0..m {
+                for j in 0..n {
+                    if grid[i][j] == 0 {
+                        res += 1;
+                    }
+                }
+            }
+            res + 1
+        };
+        let mut route = HashSet::new();
+        Self::dfs(&grid, (m - 1, n - 1), cur, target, &mut route)
+    }
+
+    fn dfs(
+        grid: &[Vec<i32>],
+        bounds: (usize, usize),
+        cur: (usize, usize),
+        target: usize,
+        route: &mut HashSet<(usize, usize)>,
+    ) -> i32 {
+        let (m, n) = bounds;
+        let (i, j) = cur;
+
+        if grid[i][j] == 2 && route.len() == target {
+            return 1;
+        }
+        route.insert(cur);
+        let mut try_seek = |(a, b): (usize, usize)| {
+            if grid[i][j] == -1 || route.contains(&(a, b)) {
+                return 0;
+            };
+            return Self::dfs(grid, bounds, (a, b), target, route);
+        };
+        let mut res = 0;
+        if i > 0 {
+            res += try_seek((i - 1, j));
+        }
+        if i < m {
+            res += try_seek((i + 1, j));
+        }
+        if j > 0 {
+            res += try_seek((i, j - 1));
+        }
+        if j < n {
+            res += try_seek((i, j + 1));
+        }
+        route.remove(&cur);
+        res
+    }
+}
