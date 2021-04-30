de6f8587fdef9056ee2295341ab8a30a7eb6ee0d
duan <wddwyss@gmail.com>
2021-04-30 22:59:20 +0900

    .

---
+++ critical-connections-in-a-network/src/lib.rs
@@ -0,0 +1,6 @@
+pub struct Solution;
+impl Solution {
+    pub fn critical_connections(n: i32, connections: Vec<Vec<i32>>) -> Vec<Vec<i32>> {
+        // An edge is a critical connection, if and only if it is not in a cycle.
+    }
+}
