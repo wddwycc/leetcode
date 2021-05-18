bcbc0ba8f22482d55b8a234dedf9cf9eb611b165
duan <wddwyss@gmail.com>
2021-05-18 20:40:50 +0900

    .

---
+++ find-duplicate-file-in-system/src/lib.rs
@@ -0,0 +1,21 @@
+use std::collections::HashMap;
+
+pub struct Solution;
+impl Solution {
+    pub fn find_duplicate(paths: Vec<String>) -> Vec<Vec<String>> {
+        let mut res: HashMap<String, Vec<String>> = HashMap::new();
+        for path in paths {
+            let parts = path.split(" ").collect::<Vec<_>>();
+            let p1 = parts[0].clone();
+            for i in 1..parts.len() {
+                let parts2 = parts[i].split("(").collect::<Vec<_>>();
+                let p2 = parts2[0];
+                let content = parts2[1].split(")").collect::<Vec<_>>()[0].to_owned();
+                res.entry(content)
+                    .or_insert(vec![])
+                    .push(format!("{}/{}", p1, p2));
+            }
+        }
+        res.values().filter(|a| a.len() > 1).cloned().collect()
+    }
+}
