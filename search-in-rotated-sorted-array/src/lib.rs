d21a6e3e790745773b6459c9ea643b297e568f2c
duan <wddwyss@gmail.com>
2021-06-02 10:30:15 +0900

    opt

--- search-in-rotated-sorted-array/src/lib.rs
+++ search-in-rotated-sorted-array/src/lib.rs
@@ -1,27 +1,40 @@
 pub struct Solution;
 impl Solution {
-    pub fn search(mut nums: Vec<i32>, target: i32) -> i32 {
-        let offset = (|| {
-            for i in 1..nums.len() {
-                if nums[i] < nums[i - 1] {
-                    return i;
+    // O(nlogn)
+    pub fn search(nums: Vec<i32>, target: i32) -> i32 {
+        let mut l = 0;
+        let mut r = nums.len() - 1;
+        while l <= r {
+            let mid = l + (r - l) / 2;
+            // case 1: hit
+            if nums[mid] == target {
+                return mid as i32;
+            }
+            // case 2: mid is in the rotated section
+            if nums[mid] <= nums[r] {
+                if target > nums[mid] && target <= nums[r] {
+                    l = mid + 1;
+                } else {
+                    if mid >= 1 {
+                        r = mid - 1;
+                    } else {
+                        return -1;
+                    }
                 }
             }
-            return 0;
-        })();
-        nums.rotate_left(offset);
-        if let Ok(i) = nums.binary_search(&target) {
-            return ((i + offset) % nums.len()) as i32;
-        } else {
-            return -1;
+            // case 3: mid is in the non-rotated section
+            else {
+                if target < nums[mid] && target >= nums[l] {
+                    if mid >= 1 {
+                        r = mid - 1;
+                    } else {
+                        return -1;
+                    }
+                } else {
+                    l = mid + 1;
+                }
+            }
         }
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+        -1
     }
 }
