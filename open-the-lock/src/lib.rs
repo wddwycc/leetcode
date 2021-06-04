77d6d97cbe257185ff5f09cb0eaa321ad3971942
duan <wddwyss@gmail.com>
2021-06-04 21:31:06 +0900

    opt with bitmask

--- open-the-lock/src/lib.rs
+++ open-the-lock/src/lib.rs
@@ -1,90 +1,59 @@
 use std::collections::{HashSet, VecDeque};
 
-type Code = (usize, usize, usize, usize);
-
 pub struct Solution;
 impl Solution {
-    fn parse_code(src: String) -> Code {
-        let mut iter = src
-            .chars()
-            .into_iter()
-            .map(|a| a.to_string().parse::<usize>().unwrap());
-        (
-            iter.next().unwrap(),
-            iter.next().unwrap(),
-            iter.next().unwrap(),
-            iter.next().unwrap(),
-        )
-    }
-
-    fn move_up(src: usize) -> usize {
-        if src == 9 {
-            0
-        } else {
-            src + 1
+    // NOTE: use 4 bits per wheel to store pos, totally 16 bits
+    fn bitmask_pos(pos: String) -> u16 {
+        let mut res = 0;
+        for b in pos.bytes() {
+            res = (res << 4) + (b - b'0') as u16;
         }
-    }
-
-    fn move_down(src: usize) -> usize {
-        if src == 0 {
-            9
-        } else {
-            src - 1
-        }
-    }
-
-    fn moves(src: Code) -> Vec<Code> {
-        vec![
-            (Self::move_up(src.0), src.1, src.2, src.3),
-            (Self::move_down(src.0), src.1, src.2, src.3),
-            (src.0, Self::move_up(src.1), src.2, src.3),
-            (src.0, Self::move_down(src.1), src.2, src.3),
-            (src.0, src.1, Self::move_up(src.2), src.3),
-            (src.0, src.1, Self::move_down(src.2), src.3),
-            (src.0, src.1, src.2, Self::move_up(src.3)),
-            (src.0, src.1, src.2, Self::move_down(src.3)),
-        ]
+        res
     }
 
     pub fn open_lock(deadends: Vec<String>, target: String) -> i32 {
-        let deadends: HashSet<Code> = deadends.into_iter().map(Self::parse_code).collect();
-        let target = Self::parse_code(target);
-
+        let deadends = deadends
+            .into_iter()
+            .map(Self::bitmask_pos)
+            .collect::<HashSet<_>>();
+        let start = Self::bitmask_pos(target);
+        // BFS
         let mut visited = HashSet::new();
-        let mut step = 0;
-        let mut queue: VecDeque<Code> = VecDeque::new();
-        queue.push_back((0, 0, 0, 0));
-        loop {
-            let queue_len = queue.len();
-            if queue_len == 0 {
-                return -1;
-            }
-            for _ in 0..queue_len {
-                let root = queue.pop_front().unwrap();
-                if deadends.contains(&root) {
-                    return -1;
+        let mut queue = VecDeque::new();
+        queue.push_back(start);
+        let mut steps = 0;
+        while queue.len() > 0 {
+            for _ in 0..queue.len() {
+                let pos = queue.pop_front().unwrap();
+                if pos == 0 {
+                    return steps;
                 }
-                if root == target {
-                    return step;
-                }
-                for next in Self::moves(root) {
-                    if visited.contains(&next) || deadends.contains(&next) {
-                        continue;
+                // try go every directions
+                for offset in 0..4 {
+                    let bit_offset = offset * 4;
+                    let v = ((15 << bit_offset) & pos) >> bit_offset;
+                    let next_pos1 = if v == 9 {
+                        pos - (9 << bit_offset)
+                    } else {
+                        pos + (1 << bit_offset)
+                    };
+                    let next_pos2 = if v == 0 {
+                        pos + (9 << bit_offset)
+                    } else {
+                        pos - (1 << bit_offset)
+                    };
+                    if !visited.contains(&next_pos1) && !deadends.contains(&next_pos1) {
+                        visited.insert(next_pos1);
+                        queue.push_back(next_pos1)
                     }
-                    visited.insert(next);
-                    queue.push_back(next);
+                    if !visited.contains(&next_pos2) && !deadends.contains(&next_pos2) {
+                        visited.insert(next_pos2);
+                        queue.push_back(next_pos2);
+                    }
                 }
             }
-            step += 1;
+            steps += 1;
         }
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-    #[test]
-    fn it_works() {
-        assert_eq!(Solution::parse_code("0000".to_string()), (0, 0, 0, 0));
+        -1
     }
 }
