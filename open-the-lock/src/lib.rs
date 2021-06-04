a0c2f41add2f5ed050e02cfd4cb7d8b4085e572d
duan <wddwyss@gmail.com>
2021-06-04 21:45:10 +0900

    opt

--- open-the-lock/src/lib.rs
+++ open-the-lock/src/lib.rs
@@ -12,45 +12,38 @@ impl Solution {
     }
 
     pub fn open_lock(deadends: Vec<String>, target: String) -> i32 {
-        let deadends = deadends
+        let target = Self::bitmask_pos(target);
+        let mut visited = deadends
             .into_iter()
             .map(Self::bitmask_pos)
             .collect::<HashSet<_>>();
-        let start = Self::bitmask_pos(target);
-        // BFS
-        let mut visited = HashSet::new();
         let mut queue = VecDeque::new();
-        queue.push_back(start);
+        queue.push_back(0);
         let mut steps = 0;
         while queue.len() > 0 {
             for _ in 0..queue.len() {
                 let pos = queue.pop_front().unwrap();
-                if pos == 0 {
+                if pos == target {
                     return steps;
                 }
-                // try go every directions
+                if visited.contains(&pos) {
+                    continue;
+                }
                 for offset in 0..4 {
                     let bit_offset = offset * 4;
                     let v = ((15 << bit_offset) & pos) >> bit_offset;
-                    let next_pos1 = if v == 9 {
+                    queue.push_back(if v == 9 {
                         pos - (9 << bit_offset)
                     } else {
                         pos + (1 << bit_offset)
-                    };
-                    let next_pos2 = if v == 0 {
+                    });
+                    queue.push_back(if v == 0 {
                         pos + (9 << bit_offset)
                     } else {
                         pos - (1 << bit_offset)
-                    };
-                    if !visited.contains(&next_pos1) && !deadends.contains(&next_pos1) {
-                        visited.insert(next_pos1);
-                        queue.push_back(next_pos1)
-                    }
-                    if !visited.contains(&next_pos2) && !deadends.contains(&next_pos2) {
-                        visited.insert(next_pos2);
-                        queue.push_back(next_pos2);
-                    }
+                    });
                 }
+                visited.insert(pos);
             }
             steps += 1;
         }
