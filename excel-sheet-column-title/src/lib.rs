84746bf2516a57a9270be6f321d8dc7506e6438b
duan <wddwyss@gmail.com>
2021-06-06 22:04:39 +0900

    .

---
+++ excel-sheet-column-title/src/lib.rs
@@ -0,0 +1,15 @@
+use std::str;
+
+pub struct Solution;
+impl Solution {
+    pub fn convert_to_title(mut column_number: i32) -> String {
+        if column_number == 0 {
+            return "".to_owned();
+        }
+        column_number -= 1;
+        let mut ans = Self::convert_to_title(column_number / 26);
+        let b = b'A' + (column_number % 26) as u8;
+        ans.push(b as char);
+        ans
+    }
+}
