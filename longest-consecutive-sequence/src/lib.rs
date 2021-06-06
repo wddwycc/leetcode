03309d417eb7cf73c188b96b00a82cf069a37cfc
duan <wddwyss@gmail.com>
2021-06-06 19:17:54 +0900

    opt

--- longest-consecutive-sequence/src/lib.rs
+++ longest-consecutive-sequence/src/lib.rs
@@ -3,14 +3,14 @@ use std::collections::HashSet;
 pub struct Solution;
 impl Solution {
     pub fn longest_consecutive(nums: Vec<i32>) -> i32 {
-        let hashset = nums.iter().map(|&a| a).collect::<HashSet<_>>();
+        let nums = nums.into_iter().collect::<HashSet<_>>();
         let mut ans = 0;
-        for num in nums {
-            if hashset.contains(&(num - 1)) {
+        for &num in &nums {
+            if nums.contains(&(num - 1)) {
                 continue;
             }
             let mut offset = 1;
-            while hashset.contains(&(num + offset)) {
+            while nums.contains(&(num + offset)) {
                 offset += 1;
             }
             ans = ans.max(offset);
