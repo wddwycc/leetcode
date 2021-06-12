31d8f0742d8897f80864a76edbef6e2b7ce97c69
duan <wddwyss@gmail.com>
2021-06-12 23:35:54 +0900

    opt

--- house-robber/src/lib.rs
+++ house-robber/src/lib.rs
@@ -9,7 +9,7 @@ impl Solution {
         let mut pre2 = 0;
         let mut pre1 = 0;
         for num in nums {
-            let next_pre1 = std::cmp::min(num + pre2, pre1);
+            let next_pre1 = std::cmp::max(num + pre2, pre1);
             pre2 = pre1;
             pre1 = next_pre1;
         }
