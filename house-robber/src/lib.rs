67cdf31c45ce6d2197d8809b06105d4f8561a8b1
duan <wddwyss@gmail.com>
2021-06-12 23:33:01 +0900

    opt

--- house-robber/src/lib.rs
+++ house-robber/src/lib.rs
@@ -1,26 +1,18 @@
 pub struct Solution;
 impl Solution {
     pub fn rob(nums: Vec<i32>) -> i32 {
-        let n = nums.len();
-        if n == 0 {
-            return 0;
-        }
-        if n == 1 {
-            return nums[0];
-        }
-        // dp[i] => when robber start at i, max profit he can achieve
+        // dp[i] => when robber reaches i, max profit he can achieve
         // dp[i] = max {
-        //     nums[i] + max { dp[i + 2] + ... dp[n - 1] },
-        //     max { dp[i + 1]... dp[n - 1] }
+        //     nums[i] + dp[i - 2],
+        //     dp[i - 1]
         // }
-        let mut rob_cur_max = nums[n - 2];
-        let mut skip_cur_max = nums[n - 1];
-        for i in (0..(n - 2)).rev() {
-            let new_rob_cur_max = nums[i] + skip_cur_max;
-            let new_skip_cur_max = std::cmp::max(rob_cur_max, skip_cur_max);
-            rob_cur_max = new_rob_cur_max;
-            skip_cur_max = new_skip_cur_max;
+        let mut pre2 = 0;
+        let mut pre1 = 0;
+        for num in nums {
+            let next_pre1 = std::cmp::min(num + pre2, pre1);
+            pre2 = pre1;
+            pre1 = next_pre1;
         }
-        std::cmp::max(rob_cur_max, skip_cur_max)
+        pre1
     }
 }
