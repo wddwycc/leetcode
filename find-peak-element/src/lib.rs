90b5d42dc845cb9089d61a0c164e54c8db6e17dc
duan <wddwyss@gmail.com>
2021-06-17 22:25:46 +0900

    .

---
+++ find-peak-element/src/lib.rs
@@ -0,0 +1,18 @@
+pub struct Solution;
+impl Solution {
+    pub fn find_peak_element(nums: Vec<i32>) -> i32 {
+        // find the first el that
+        let n = nums.len();
+        let mut l = 0;
+        let mut r = n - 1;
+        while l < r {
+            let mid = l + (r - l) / 2;
+            if nums[mid] > nums[mid + 1] {
+                r = mid;
+            } else {
+                l = mid + 1;
+            }
+        }
+        l as i32
+    }
+}
