40a968ed68c2fa2a43da2a0dd115109264f8efde
duan <wddwyss@gmail.com>
2021-06-17 22:26:01 +0900

    .

--- find-peak-element/src/lib.rs
+++ find-peak-element/src/lib.rs
@@ -1,7 +1,6 @@
 pub struct Solution;
 impl Solution {
     pub fn find_peak_element(nums: Vec<i32>) -> i32 {
-        // find the first el that
         let n = nums.len();
         let mut l = 0;
         let mut r = n - 1;
