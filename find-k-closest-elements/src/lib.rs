b66b40f4e3a6ecbca449286247daba761c4d5217
duan <wddwyss@gmail.com>
2021-06-19 12:38:42 +0900

    .

---
+++ find-k-closest-elements/src/lib.rs
@@ -0,0 +1,17 @@
+pub struct Solution;
+impl Solution {
+    pub fn find_closest_elements(arr: Vec<i32>, k: i32, x: i32) -> Vec<i32> {
+        let k = k as usize;
+        let mut l = 0;
+        let mut r = arr.len() - k;
+        while l < r {
+            let mid = l + (r - l) / 2;
+            if x - arr[mid] > arr[mid + k] - x {
+                l = mid + 1;
+            } else {
+                r = mid;
+            }
+        }
+        arr[l..(l + k)].into()
+    }
+}
