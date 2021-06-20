d6aea5c481030b2db268b5e0a55a8454e178a069
duan <wddwyss@gmail.com>
2021-06-20 22:54:47 +0900

    opt with bitmask

--- minimum-absolute-difference-queries/src/lib.rs
+++ minimum-absolute-difference-queries/src/lib.rs
@@ -1,7 +1,8 @@
+// https://en.wikipedia.org/wiki/Segment_tree
 pub struct SegmentTree {
     pub start: usize,
     pub end: usize,
-    pub dist: Vec<bool>,
+    pub bitmask: u128,
     pub left: Option<Box<SegmentTree>>,
     pub right: Option<Box<SegmentTree>>,
 }
@@ -10,12 +11,11 @@ impl SegmentTree {
     // O(n)
     pub fn new(start: usize, end: usize, vals: &[i32]) -> Self {
         if start == end {
-            let mut dist = vec![false; 100];
-            dist[vals[start] as usize - 1] = true;
+            let bitmask = 1 << (vals[start] - 1);
             return Self {
                 start,
                 end,
-                dist,
+                bitmask,
                 left: None,
                 right: None,
             };
@@ -23,24 +23,20 @@ impl SegmentTree {
         let mid = start + (end - start) / 2;
         let left = Self::new(start, mid, vals);
         let right = Self::new(mid + 1, end, vals);
-        let mut dist = vec![false; 100];
-        for i in 0..100 {
-            dist[i] = left.dist[i] || right.dist[i];
-        }
         Self {
             start,
             end,
-            dist,
+            bitmask: left.bitmask | right.bitmask,
             left: Some(Box::new(left)),
             right: Some(Box::new(right)),
         }
     }
 
     // O(logn)
-    pub fn query(&self, start: usize, end: usize) -> Vec<bool> {
+    pub fn query(&self, start: usize, end: usize) -> u128 {
         // NOTE: Exact match
         if self.start == start && self.end == end {
-            return self.dist.clone();
+            return self.bitmask;
         }
         let mid = self.start + (self.end - self.start) / 2;
         // NOTE: Range on the left
@@ -53,11 +49,7 @@ impl SegmentTree {
         } else {
             let left = self.left.as_ref().unwrap().query(start, mid);
             let right = self.right.as_ref().unwrap().query(mid + 1, end);
-            let mut dist = vec![false; 100];
-            for i in 0..100 {
-                dist[i] = left[i] || right[i];
-            }
-            return dist;
+            return left | right;
         }
     }
 }
@@ -71,11 +63,11 @@ impl Solution {
             .map(|r| {
                 let start = r[0] as usize;
                 let end = r[1] as usize;
-                let dist = tree.query(start, end);
+                let bitmask = tree.query(start, end);
                 let mut prev: Option<usize> = None;
                 let mut res: Option<usize> = None;
                 for i in 0..100 {
-                    if !dist[i] {
+                    if (bitmask >> i) % 2 == 0 {
                         continue;
                     }
                     match (prev, res) {
