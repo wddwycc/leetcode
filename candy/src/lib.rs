ad52e6caf4295d42b3d000772164054c9d613252
duan <wddwyss@gmail.com>
2021-06-27 22:18:20 +0900

    opt

--- candy/src/lib.rs
+++ candy/src/lib.rs
@@ -2,36 +2,17 @@ pub struct Solution;
 impl Solution {
     pub fn candy(ratings: Vec<i32>) -> i32 {
         let n = ratings.len();
-        let l2r = {
-            let mut res = vec![1; n];
-            let mut acc = 0;
-            for i in 1..n {
-                if ratings[i] <= ratings[i - 1] {
-                    acc = 0;
-                    continue;
-                }
-                acc += 1;
-                res[i] += acc;
+        let mut ans = vec![1; n];
+        for i in 1..n {
+            if ratings[i] > ratings[i - 1] {
+                ans[i] = ans[i - 1] + 1;
             }
-            res
-        };
-        let r2l = {
-            let mut res = vec![1; n];
-            let mut acc = 0;
-            for i in (0..(n - 1)).rev() {
-                if ratings[i] <= ratings[i + 1] {
-                    acc = 0;
-                    continue;
-                }
-                acc += 1;
-                res[i] += acc;
-            }
-            res
-        };
-        let mut ans = 0;
-        for i in 0..n {
-            ans += std::cmp::max(l2r[i], r2l[i]);
         }
-        ans
+        for i in (0..(n - 1)).rev() {
+            if ratings[i] > ratings[i + 1] {
+                ans[i] = std::cmp::max(ans[i + 1] + 1, ans[i]);
+            }
+        }
+        ans.into_iter().sum()
     }
 }
