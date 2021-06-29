0a88ec25183f4bb3359833b573d92870bc112d63
duan <wddwyss@gmail.com>
2021-06-30 00:14:07 +0900

    opt

--- find-k-th-smallest-pair-distance/src/lib.rs
+++ find-k-th-smallest-pair-distance/src/lib.rs
@@ -1,6 +1,6 @@
-pub struct Solution
-impl Solution {
-    // O(n^2)
+// O(n^2), brute-force, calc all distances and enumerate with bucket sorting
+pub struct Solution1;
+impl Solution1 {
     pub fn smallest_distance_pair(mut nums: Vec<i32>, k: i32) -> i32 {
         nums.sort();
         let n = nums.len();
@@ -19,8 +19,42 @@ impl Solution {
                 k -= buckets[cur];
                 cur += 1;
             } else {
-                return cur as i32;   
+                return cur as i32;
             }
         }
+    }
+}
+
+pub struct Solution2;
+impl Solution2 {
+    // O(nlogn)
+    pub fn smallest_distance_pair(mut nums: Vec<i32>, k: i32) -> i32 {
+        nums.sort();
+        let n = nums.len();
+        let k = k as usize;
+
+        // given function f(i) = ct (nums of pairs with distance lte i)
+        // binary search i range from [0, max_distance]
+        // find first i that satisfies f(i) >= k
+        let mut l = 0;
+        let mut r = nums[n - 1] - nums[0];
+
+        while l < r {
+            let mid = l + (r - l) / 2;
+            let mut ct = 0;
+            for i in 0..(n - 1) {
+                let fst_v_gte_nums_i = nums[i] + mid + 1;
+                match &nums[(i + 1)..].binary_search(&fst_v_gte_nums_i) {
+                    Ok(j) => ct += j,
+                    Err(j) => ct += j,
+                }
+            }
+            if ct < k {
+                l = mid + 1;
+            } else {
+                r = mid;
+            }
+        }
+        l
     }
 }
